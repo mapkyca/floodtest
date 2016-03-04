459681b6525a7bfc29407b6c750d505f8a3a9b1c
Marcus Povey <marcus@dushka.co.uk>
2016-03-04 15:00:27 +0000

    Correcting json regex

--- getchartdata.sh
+++ getchartdata.sh
@@ -5,5 +5,5 @@ if [ $# -eq 0 ]; then
     echo "Usage ./getchartdata.sh <url>"
     exit 1
 fi
-curl -s $1 | grep root.chartData | grep -oP '(\{.*\})+' |  python -m json.tool
+curl -s $1 | grep chart | grep -oP '(\{.*\})+' |  python -m json.tool
 
