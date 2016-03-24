e609b79e8534984362ec72bdb9b3c1bde81b7356
Marcus Povey <marcus@marcus-povey.co.uk>
2016-03-24 12:39:50 +0000

    Updating to catch station details

--- getchartdata.sh
+++ getchartdata.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 if [ $# -eq 0 ]; then
-    echo "Grab the JSON chart data object from the website."
+    echo "Grab the JSON station data object from the website."
     echo "Usage ./getchartdata.sh <url>"
     exit 1
 fi
-curl -s $1 | grep chart | grep -oP '(\{.*\})+' |  python -m json.tool
+curl -s $1 | grep station | grep -oP '(\{.*\})+' |  python -m json.tool
 
